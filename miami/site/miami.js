2f55b19f3db45acb31bf6b903e777bc99be1c0af
Billy Joinville <bxj293@miami.edu>
2024-10-25 12:58:54 -0400

    Midterm Project - In Review
    this is what I have currently. I believe that I have filled all requirements, but I am not sure how to put the pages together.

--- miami/site/miami.js
+++ miami/site/miami.js
@@ -3,6 +3,9 @@
 const express = require('express')
 
 const app = express()
+// specify static routes
+
+app.use(express.static('public'))
 
 
 // import a package for handlebars called express-handlebars
@@ -18,46 +21,59 @@ app.set('view engine', 'handlebars')
 
 // store the express server in a variable called app
 const PORT = process.env.PORT || 3000
+
+// Import app-wide data
+
+const gallery = require('./data/gallery.json')
 // process routes 
 
 app.get('/', (request, response) => {
 
-   response.render('home')
+   console.log(gallery)
+
+   const data = require("./data/home-data.json")
+   response.render('landing', {
+   gallery,
+   data
+
+   })
 })
 
-app.get('/about', (request, response) => {
+app.get('/economy', (request, response) => {
 
-   response.render('about')
+   response.render('page',
+    {title: "Economy in Los Angeles",
+    abstract: "All you need to know about Los Angeles. From the Hollywood sign to the Santa Monica Pier, there is something for everyone.",
+    }
+   )
 
 })
+
+app.get('/entertainment', (request, response) => {
+
+    response.render('page',
+     {title: "Entertainment in Los Angeles",
+     abstract: "Here, you can find the best entertainment in Los Angeles. From the Hollywood sign to the Santa Monica Pier, there is something for everyone.",
+     }
+    )
+ 
+ })
+
 
 app.get('/weather', (request, response) => {
 
     response.type('text/plain')
-    response.send('Get more information on Miami weather and climate')
+    response.send('Get more information on Los Angeles weather and climate and plan your trip accordingly')
 
 })
 
-app.get('/location', (request, response) => {
+app.get('/gallery', (request, response) => {
     
         response.type('text/plain')
-        response.send('Explore Miami Desstinations, such as Miami Beach, Coconut Grove, and Coral Gables')
+        response.send('This is a collection of images from Los Angeles. Explore some of the gorgeous views of the city.')
     
     })
 
-app.get('/contact', (request, response) => {
-
-    response.type('text/plain')
-    response.send('Contact Miami Government')
-
-})
-
-app.get('/history', (req, res) => {
-
-    res.type('text/plain')
-    res.send('Learn more about Miami history')
-
-})
 
 // handle the error FIRST
 // default responses for anything that does not match the routes above (below) NOT FOUND!
@@ -75,14 +91,14 @@ app.get('/error', (req, res) => {
 
     response.type('text/plain')
     response.status(500)
-    response.send('Hi there, this is a test!')
+    response.send('Hi there, this a server error!')
 
 })
     
 
 // SERVER ERROR :(
     app.use((error,request,response,next)=>{
-    
+    console.log(error.message)
         response.status(500)
         response.render('500')
     })
@@ -92,3 +108,15 @@ app.get('/error', (req, res) => {
         console.log(`Express is running on http://localhost:${PORT}`)
         console.log('Press Ctrl-C to terminate...')
     })
+
+
+
+
+    //NOTES
+
+    //Section descriptions:
+    // About: 1  short paragraph per page
+    // Services: At least 4 services per page
+    // Packages: Show at least 6 packages per page
+    // Gallery: Show at least 16 images per page
+
